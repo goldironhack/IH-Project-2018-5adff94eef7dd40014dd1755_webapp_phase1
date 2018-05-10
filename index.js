9def76e7d2756dd81b5b32f4495c7547de490670
oscarcamilopulidop <ocpulidop@unal.edu.co>
2018-05-09 22:22:07 -0400

    Automatic commit. Done before go to queltrics

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
+/*JavaScript Google*/      
+var map;
+      function nyMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+          center: {lat:40.729100  , lng:  -73.996500},
+          zoom: 9.8
+        });
+        
+          map.data.loadGeoJson(
+                    'http://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson');
+          
+          map.data.setStyle({
+              fillColor: 'red',
+              strokeWeight: 1
+          });
+      }
+      
+   
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
-    });
-})();
